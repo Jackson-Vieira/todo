75e83cdda056922582094f2d4f2619b55bee8cda
Jackson-Vieira <jacksonvieiira12@gmail.com>
2023-03-18 19:46:17 -0400

    dont allow update a todo-item if already exist

--- src/usecases/update-todo-item.ts
+++ src/usecases/update-todo-item.ts
@@ -7,11 +7,16 @@ export class UpdateTodoItem {
     item_description: string,
     new_description: string
   ): Promise<void> {
+    this.todoListRepository.setStatus("Updating todo item...");
     const todoList = await this.todoListRepository.findByEmail(user_email);
     if (!todoList) {
       throw new Error("User does not exist");
     }
+    if(todoList.find_by_description(new_description)){
+      throw new Error("Todo item already exists");
+    }
     todoList.change_description(item_description, new_description);
     await this.todoListRepository.update(user_email, todoList);
+    this.todoListRepository.setStatus("Ready");
   }
 }
